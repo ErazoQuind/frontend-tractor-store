506f2a8b92b8f92910cd4ec49ff3696b01cb7d9c
ErazoQuind <brayan.erazo@quind.io>
2026-06-04 04:16:10 -0500

    chore(design-system): actualizar barril de exportaciones para nuevos componentes

--- packages/ts-design-system/src/index.ts
+++ packages/ts-design-system/src/index.ts
@@ -1,3 +1,7 @@
 export * from './lib/ts-design-system/ts-design-system';
+export * from './lib/button/button';
+export * from './lib/product-card/product-card';
 
-export * from './lib/button';
+export * from './lib/variant-option/variant-option';
+
+export * from './lib/mini-cart/mini-cart';
